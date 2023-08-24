2e9dfe9c912f99b1632c059608c5bc95c15dd0c9
SafaFarah <kangjisook13@gmail.com>
2023-08-24 16:29:11 -0700

    Adding test dir

---
+++ test/_free.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * _free - frees the stack;
+ * @pointer: pointer to the stack.
+ */
+void _free(stack_t *pointer)
+{
+	stack_t *temp;
+
+	temp = pointer;
+	while (pointer != NULL)
+	{
+		temp = pointer->next;
+		free(pointer);
+		pointer = temp;
+	}
+}
